ce886f6a2f671f0da462217ca9da08ed9462462e
Daniel Kim <daniel.kim1116@gmail.com>
2016-06-26 20:12:56 -0700

    composition finished

---
+++ composition/src/com/dkim/Vehicle.java
@@ -0,0 +1,12 @@
+package com.dkim;
+
+/**
+ * Created by dkim on 6/26/16.
+ */
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
